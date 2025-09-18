d15d1a0ef44e4070a0fbd1b1d3c66fd30be3139d
Yuriy Pilipenko <yurik2061@gmail.com>
2025-09-19 01:35:02 +0300

    first reqest to openai

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,5 +2,8 @@ import { generateText } from "ai";
 import { openai } from "@ai-sdk/openai";
 
 export async function POST(req:Request) {
-  
+  generateText({
+    model: openai('gpt-3.5-turbo'),
+    prompt: 'explain what llm is in simple trms'
+  })
 }
