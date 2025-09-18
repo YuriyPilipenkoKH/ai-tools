68df4398d33dae9dcca8c280b100b270217105ab
Yuriy Pilipenko <yurik2061@gmail.com>
2025-09-19 01:39:33 +0300

    fixed api route

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,9 +1,11 @@
 import { generateText } from "ai";
 import { openai } from "@ai-sdk/openai";
 
-export async function POST(req:Request) {
-  generateText({
+export async function POST() {
+  const {text}= await generateText({
     model: openai('gpt-3.5-turbo'),
     prompt: 'explain what llm is in simple trms'
   })
+
+  return Response.json({text})
 }
