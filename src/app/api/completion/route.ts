17bb544d9da34bec3c2728ac365163e538d9e241
Yuriy Pilipenko <yurik2061@gmail.com>
2025-09-19 01:55:58 +0300

    exceeded your current quota

--- src/app/api/completion/route.ts
+++ src/app/api/completion/route.ts
@@ -3,7 +3,7 @@ import { openai } from "@ai-sdk/openai";
 
 export async function POST() {
   const {text}= await generateText({
-    model: openai('gpt-3.5-turbo'),
+    model: openai('gpt-4o-mini'),
     prompt: 'explain what llm is in simple trms'
   })
 
