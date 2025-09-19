79a58a45289524c5e3275245494c398694c62486
Yuriy Pilipenko <yurik2061@gmail.com>
2025-09-19 12:59:12 +0300

    controlled input is done

--- src/app/ui/completion/page.tsx
+++ src/app/ui/completion/page.tsx
@@ -1,11 +1,17 @@
 "use client"
 
+import { useState } from "react"
+
 export default function CompletionPage () {
+  const [prompt, setPrompt] = useState("")
+
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
       <form className="fixed bottom-0 w-full max-w-md mx-auto left-0 right-0 p-4 bg-zinc-50 dark:bg-zinc-950 border-t border-zinc-200 dark:border-zinc-800 shadow-lg">
         <div className="flex gap-2">
           <input 
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
           type="text" 
           placeholder="how can I help You?"
           className="flex-1 dark:bg-zinc-800 p-2 border border-zinc-300 dark:border-zinc-700 rounded shadow-xl"/>
