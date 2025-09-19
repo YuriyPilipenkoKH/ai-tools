f447849c196d0676283c72fa87029015940f5de5
Yuriy Pilipenko <yurik2061@gmail.com>
2025-09-19 12:50:34 +0300

    markup for completion page

--- src/app/ui/completion/page.tsx
+++ src/app/ui/completion/page.tsx
@@ -1,5 +1,14 @@
 "use client"
 
 export default function CompletionPage () {
-  
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" placeholder="how can I help you?"/>
+          <button type="submit">Send</button>
+        </div>
+      </form>
+    </div>
+  )
 }
