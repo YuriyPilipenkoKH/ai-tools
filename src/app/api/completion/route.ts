4dd877de258c445582986900988eba32cb6850dd
Yuriy Pilipenko <yurik2061@gmail.com>
2025-09-19 21:30:45 +0300

    submitted successfully

--- src/app/api/completion/route.ts
+++ src/app/api/completion/route.ts
@@ -3,14 +3,13 @@ import { openai } from "@ai-sdk/openai";
 
 export async function POST(req: Request) {
   try {
-    
     const {prompt} = await  req.json()
     if (!prompt || typeof prompt !== 'string') {
       return Response.json({error: 'Invalid prompt'}, {status: 400})
     }
     const {text}= await generateText({
       model: openai('gpt-4'),
-      prompt: 'explain what llm is in simple trms'
+      prompt,
     })
   
     return Response.json({text})
--- src/app/ui/completion/page.tsx
+++ src/app/ui/completion/page.tsx
@@ -45,7 +45,9 @@ export default function CompletionPage () {
           ) : completion ? (
             <div className="whitespace-pre-wrap">{completion}</div>
           ) : null}
-      <form className="fixed bottom-0 w-full max-w-md mx-auto left-0 right-0 p-4 bg-zinc-50 dark:bg-zinc-950 border-t border-zinc-200 dark:border-zinc-800 shadow-lg">
+      <form 
+      onSubmit={complete}
+      className="fixed bottom-0 w-full max-w-md mx-auto left-0 right-0 p-4 bg-zinc-50 dark:bg-zinc-950 border-t border-zinc-200 dark:border-zinc-800 shadow-lg">
         <div className="flex gap-2">
           <input 
           value={prompt}
