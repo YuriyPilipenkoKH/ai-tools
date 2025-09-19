4ef12ffbcf17c236159a9ca76eba331100bbe9ab
Yuriy Pilipenko <yurik2061@gmail.com>
2025-09-19 21:26:26 +0300

    wrapping in trycatch

--- src/app/api/completion/route.ts
+++ src/app/api/completion/route.ts
@@ -2,14 +2,20 @@ import { generateText } from "ai";
 import { openai } from "@ai-sdk/openai";
 
 export async function POST(req: Request) {
-  const { prompt } = await req.json()
-  if (!prompt || typeof prompt !== 'string') {
-    return Response.json({ error: 'Invalid prompt' }, { status: 400 })
+  try {
+    
+    const {prompt} = await  req.json()
+    if (!prompt || typeof prompt !== 'string') {
+      return Response.json({error: 'Invalid prompt'}, {status: 400})
+    }
+    const {text}= await generateText({
+      model: openai('gpt-4'),
+      prompt: 'explain what llm is in simple trms'
+    })
+  
+    return Response.json({text})
+  } catch (error) {
+    console.log(error);
+    return Response.json({error: 'Failed to generate text'}, {status: 500})
   }
-  const {text}= await generateText({
-    model: openai('o3-mini'),
-    prompt,
-  })
-
-  return Response.json({text})
 }
