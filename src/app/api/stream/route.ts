9ca4811cd539ebb13a58cc9ca77af223874df23e
Yuriy Pilipenko <yurik2061@gmail.com>
2025-09-20 07:44:35 +0300

    returning MessageStreamResponse

--- src/app/api/stream/route.ts
+++ src/app/api/stream/route.ts
@@ -1,4 +1,14 @@
+import { streamText } from "ai";
+import { openai } from "@ai-sdk/openai";
 
 export async function POST(req: Request) {
-  
+  const {prompt} = await  req.json()
+  if (!prompt || typeof prompt !== 'string') {
+    return Response.json({error: 'Invalid prompt'}, {status: 400})
+  }
+  const stream = streamText({
+    model: openai('gpt-4'),
+    prompt,
+  })
+  return stream.toUIMessageStreamResponse()
 }
