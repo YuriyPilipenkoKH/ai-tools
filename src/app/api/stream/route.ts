246aa1e30c8754cdd56fa69998adb4c66529dc90
Yuriy Pilipenko <yurik2061@gmail.com>
2025-09-20 07:46:42 +0300

    wrapped in trycatch

--- src/app/api/stream/route.ts
+++ src/app/api/stream/route.ts
@@ -2,13 +2,19 @@ import { streamText } from "ai";
 import { openai } from "@ai-sdk/openai";
 
 export async function POST(req: Request) {
-  const {prompt} = await  req.json()
-  if (!prompt || typeof prompt !== 'string') {
-    return Response.json({error: 'Invalid prompt'}, {status: 400})
+  try {
+    const {prompt} = await  req.json()
+    if (!prompt || typeof prompt !== 'string') {
+      return Response.json({error: 'Invalid prompt'}, {status: 400})
+    }
+    const stream = streamText({
+      model: openai('gpt-4'),
+      prompt,
+    })
+    return stream.toUIMessageStreamResponse()
+    
+  } catch (error) {
+        console.error(error);
+    return new Response('Failed to stream text', {status: 500})
   }
-  const stream = streamText({
-    model: openai('gpt-4'),
-    prompt,
-  })
-  return stream.toUIMessageStreamResponse()
 }
