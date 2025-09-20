b704101031887e4fe702dd8b66fbe46d9c924cb1
Yuriy Pilipenko <yurik2061@gmail.com>
2025-09-20 19:08:53 +0300

    updated stream page

--- src/app/ui/stream/page.tsx
+++ src/app/ui/stream/page.tsx
@@ -1,8 +1,66 @@
 "use client"
 
+import { useState } from "react"
+
 function StreamPage() {
+    const [prompt, setPrompt] = useState("") // state for input field
+    const [completion, setCompletion] = useState("")// state for response from API
+    const [isLoading, setIsLoading] = useState(false) // state for loading
+    const [error, setError] = useState<string | null>(null) // state for error handling
+  
+    const complete = async (e: React.FormEvent) => {
+      e.preventDefault()
+      setIsLoading(true)
+      setPrompt("")
+      try {
+        const response = await fetch('/api/completion', {
+          method: 'POST',
+          headers: {  'Content-Type': 'application/json' },
+          body: JSON.stringify({ prompt })
+        })
+        const data = await response.json()
+        if (!response.ok) {
+          throw new Error(data.error || 'Something went wrong')
+        }
+        setCompletion(data.text)
+        } 
+      catch (error) {
+        console.log(error);
+        if (error instanceof Error) {
+          setError(error.message || 'Something went wrong, please try again.')
+        }
+      }
+      finally{
+        setIsLoading(false)
+      }
+  
+    }
+  
   return (
-    <div>StreamPage</div>
+        <div className='chat-container'>
+        {error && <div className='chat-error'>{error}</div>}
+        {isLoading ? (
+            <div>Loading...</div>
+          ) : completion ? (
+            <div className='chat-loading'>{completion}</div>
+          ) : null}
+      <form 
+      onSubmit={complete}
+      className='chat-form'>
+        <div className='input-container'>
+          <input 
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+          type="text" 
+          placeholder="how can I help You streaming?"
+          className='chat-input'/>
+          <button type="submit"
+           className='chat-button'>
+            Send
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
 
