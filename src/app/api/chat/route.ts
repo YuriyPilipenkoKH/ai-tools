f7eca46f33e4e750662f7cca112f1c97f938ed5b
Yuriy Pilipenko <yurik2061@gmail.com>
2025-09-21 20:32:33 +0300

    chat route handler

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,19 @@
+import { UIMessage , streamText} from "ai";
+import { openai } from "@ai-sdk/openai";
+
+export async function POST(req: Request) {
+  try {
+    const {messages} : {messages: UIMessage[]}= await  req.json()
+    if (!messages || typeof messages !== 'string') {
+      return Response.json({error: 'Invalid prompt'}, {status: 400})
+    }
+    const stream = streamText({
+     model: openai('gpt-4o-mini'),
+      messages,
+    })
+    
+  } catch (error) {
+    console.error(error);
+    return Response.json({error: 'Failed to stream text'}, {status: 500})
+  }   
+}
