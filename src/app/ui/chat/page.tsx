bbb5b745f548107d8ff3e7afce0aa484e3a3fb6e
Yuriy Pilipenko <yurik2061@gmail.com>
2025-09-21 20:39:36 +0300

    added markup to chat page

--- src/app/ui/chat/page.tsx
+++ src/app/ui/chat/page.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+"use client"
+
+import { chatClasses } from "@/models/chatClasses"
 
 function ChatPage() {
   return (
-    <div>ChatPage</div>
+     <div className={chatClasses.container}>
+           {/* {error && <div className={chatClasses.error}>{error}</div>} */}
+           {/* {isLoading ? (
+               <div>Loading...</div>
+             ) : completion ? (
+               <div className={chatClasses.loading}>{completion}</div>
+             ) : null} */}
+         <form 
+        //  onSubmit={complete}
+         className={chatClasses.form}>
+           <div className={chatClasses.inputContainer}>
+             <input 
+            //  value={prompt}
+            //  onChange={(e) => setPrompt(e.target.value)}
+             type="text" 
+             placeholder="how can I help You?"
+             className={chatClasses.input}/>
+             <button type="submit"
+              className={chatClasses.button}>
+               Send
+             </button>
+           </div>
+         </form>
+       </div>
   )
 }
 
