b2c4cca535dc9eb2539a7577dda02508376e58dc
Yuriy Pilipenko <yurik2061@gmail.com>
2025-09-24 09:32:17 +0300

    adding schema to stream obj

--- src/app/api/structured-data/route.ts
+++ src/app/api/structured-data/route.ts
@@ -1,11 +1,13 @@
 import { streamObject } from "ai";
 import { openai } from "@ai-sdk/openai";
+import { recipeSchema } from "./schems";
 
 export async function POST(req: Request) {
     const { dish } = await req.json();
 
     const result = streamObject({
       model: openai("gpt-5-nano"),
+      schema: recipeSchema,
       prompt: `Generate a recipe for ${dish}`,
     });
 
