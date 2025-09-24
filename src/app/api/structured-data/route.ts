1b244afae794063883b15e386e2804d216b6951d
Yuriy Pilipenko <yurik2061@gmail.com>
2025-09-24 09:29:47 +0300

    got streamObject

--- src/app/api/structured-data/route.ts
+++ src/app/api/structured-data/route.ts
@@ -4,5 +4,10 @@ import { openai } from "@ai-sdk/openai";
 export async function POST(req: Request) {
     const { dish } = await req.json();
 
-    console.log({ dish });
+    const result = streamObject({
+      model: openai("gpt-5-nano"),
+      prompt: `Generate a recipe for ${dish}`,
+    });
+
+    return result.toTextStreamResponse();
 }
