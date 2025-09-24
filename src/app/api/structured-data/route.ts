8d217931520e1ca86d17c33f2e60e07356629fae
Yuriy Pilipenko <yurik2061@gmail.com>
2025-09-24 09:27:01 +0300

    checking reqest

--- src/app/api/structured-data/route.ts
+++ src/app/api/structured-data/route.ts
@@ -2,5 +2,7 @@ import { streamObject } from "ai";
 import { openai } from "@ai-sdk/openai";
 
 export async function POST(req: Request) {
+    const { dish } = await req.json();
 
+    console.log({ dish });
 }
