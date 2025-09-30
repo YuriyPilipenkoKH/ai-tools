e3688ffccb30cf44aff8122c4a7955e16ba46550
Yuriy Pilipenko <yurik2061@gmail.com>
2025-09-30 10:11:55 +0300

    completed req to openai

--- src/app/ui/structured-array/route.ts
+++ src/app/ui/structured-array/route.ts
@@ -1,8 +1,16 @@
 import { openai } from "@ai-sdk/openai";
+import { generateObject } from "ai";
 
 export async function POST(req: Request) {
   try {
     const { text } = await req.json();
+
+    const result = await generateObject({
+      model: openai("gpt-5-nano"),
+      prompt: `List the key features of ${text} in an array of objects with name and abilities properties.`,    
+      output: 'enum',
+      enum: [{ name: "string", abilities: ["string"] }],
+    })
   } catch (error) {
     
   }
