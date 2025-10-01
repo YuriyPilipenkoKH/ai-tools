5ecad09364bda7fd18f2718e9b1c6c3ae1881686
Yuriy Pilipenko <yurik2061@gmail.com>
2025-10-01 09:44:30 +0300

    retreiving vars from useObject hook

--- src/app/ui/structured-array/page.tsx
+++ src/app/ui/structured-array/page.tsx
@@ -1,6 +1,15 @@
-import React from 'react'
+"use client"
+import { useState } from "react"
+import { experimental_useObject as useObject} from "@ai-sdk/react";
+import { structuredArraySchema } from "./schema";
 
 function StructuredArrayPage() {
+    const [type, setType] = useState("");
+  
+     const { submit, object, isLoading, error, stop } = useObject({
+      api: "/api/structured-array",
+      schema: structuredArraySchema, 
+    });
   return (
     <div>StructuredArrayPage</div>
   )
